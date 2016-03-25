42d9ce41564848339310503e03fb6749c16352e1
Callum <callum.waddell@btinternet.com>
2016-03-25 22:10:25 +0000

    BareBone Classes
    empty classes

---
+++ SpyGame/Assets/Custom Assets/Scripts/Objects/NPC.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class NPC : MonoBehaviour
+{
+
+	int iId;
+	string sName;
+	
+
+
+	// Use this for initialization
+	void Start()
+	{
+		InitFactionRelations();
+
+	}
+
+	#region init functions
+	void InitFactionRelations()
+	{
+
+
+	}
+	#endregion
+	#region update functions
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	#endregion
+
+}
